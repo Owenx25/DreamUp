53dc92b8ab328360392a61f3335811dd251b0be4
Jacob Phillips <jacob_phillips@student.uml.edu>
2019-03-27 21:29:05 -0400

    Header icons
    Header icons added, menu opens to VisonCanvas right now, working on having it open pop up menu instead

--- Components/App.js
+++ Components/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { Platform } from "react-native";
 
 import DreamDashboard from './DreamDashboard';
 import DreamScreen from './DreamScreen';
@@ -11,25 +10,37 @@ import VisionCanvas from './VisionCanvas';
 import DreamFragmentScreen from './DreamFragmentScreen';
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
+
+    static navigationOptions = ({ navigation }) => ({
     title: 'DreamUp',
         headerTitleStyle: {
             textAlign:"center",
             flex:1
         },
-    headerRight: (
+    headerLeft: (
                        <Icon
-                         onPress={() => alert('This is a button!')}
-                         name="arrow-right"
-                         color="##fff"
+                         //onPress={() => this.  alert('This is a button!')}
+                         onPress={() => navigation.navigate('VisionCanvas')}
+                         name='bars'
+                         size={26}
+                         style = {{paddingLeft: 15}}
                        />
                      ),
+     headerRight: (
+                        <Icon
+                          onPress={() => alert('DreamUp 2019')}
+                          name='info-circle'
+                          size={26}
+                          style = {{paddingRight: 15}}
+                        />
+                      ),
     headerStyle: {
-      backgroundColor: '#c4941d'
-    }
-  };
-  render() {  
-    return ( 
+      backgroundColor: '#c4941d',
+      }
+    });
+
+  render() {
+    return (
       <View style={{backgroundColor: '#2b1381', flex: 1}}>
         {/* <Toolbar title='DreamUp'/> */}
         <DreamDashboard navigation={this.props.navigation}/>
@@ -50,6 +61,8 @@ const RootStack = createStackNavigator({
     headerStyle: {
       backgroundColor: '#c4941d'
     }
+  },
+  navigationOptions: {
   }
 });
 
--- Components/DreamScreen.js
+++ Components/DreamScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
 
 export default class DreamScreen extends Component {
     render() {
@@ -12,3 +12,6 @@ export default class DreamScreen extends Component {
         )
     }
 }
+
+
+      //AppRegistry.registerComponent('DreamScreen', () => DreamScreen);
