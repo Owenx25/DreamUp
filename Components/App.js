cd5ce1c20cde6b7b80b51e9b57813772c31b6b77
Jacob Phillips <jacob_phillips@student.uml.edu>
2019-04-07 15:29:10 -0400

    HomeScreen moved to new file
    helps reorganize while implementing react-navigation

--- Components/App.js
+++ Components/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Platform } from "react-native";
-
 import DreamDashboard from './DreamDashboard';
 import DreamScreen from './DreamScreen';
 import VisionCanvas from './VisionCanvas';
 import DreamFragmentScreen from './DreamFragmentScreen';
 import SettingsScreen from './SettingsScreen';
+import HomeScreen from './HomeScreen';
 
 /* Only objects like : 
   {
@@ -34,43 +32,6 @@ import SettingsScreen from './SettingsScreen';
   }
 */
 
-class HomeScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-  title: 'DreamUp',
-      headerTitleStyle: {
-          textAlign:"center",
-          flex:1
-      },
-  headerLeft: (
-                      <Icon
-                        //onPress={() => this.  alert('This is a button!')}
-                        onPress={() => navigation.navigate('SettingsScreen')}
-                        name='bars'
-                        size={26}
-                        style = {{paddingLeft: 15}}
-                      />
-                    ),
-    headerRight: (
-                      <Icon
-                        onPress={() => alert('DreamUp 2019')}
-                        name='info-circle'
-                        size={26}
-                        style = {{paddingRight: 15}}
-                      />
-                    ),
-  headerStyle: {
-    backgroundColor: '#c4941d',
-    }
-  });
-
-  render() {
-    return (
-      <View style={{backgroundColor: '#2b1381', flex: 1}}>
-        <DreamDashboard navigation={this.props.navigation}/>
-      </View>
-    );
-  }
-}
 
 const RootStack = createStackNavigator({
   Home: HomeScreen,
