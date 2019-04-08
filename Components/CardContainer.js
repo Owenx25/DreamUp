be4dc4f727e063dd04b6cf4d8d0aed8b107e4707
Owen McCormack <Owen_McCormack@student.uml.edu>
2019-04-08 10:04:48 -0400

    Message shown in CardContainers when no dreams

--- Components/CardContainer.js
+++ Components/CardContainer.js
@@ -14,14 +14,17 @@ import DreamCard from './DreamCard';
       />
     );
     render() {
+      this.props.data.length == 0 ? 
+        content = <Text style={{fontSize: 20, color: '#b300b3'}}>No dreams yet...</Text>:
+        content = <FlatList
+          horizontal={true} data={this.props.data}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />;
       return (
-        <View style={{margin: 20, height: 150, backgroundColor: '#321697'}}>
+        <View style={{margin: 20, height: 150, backgroundColor: '#321697', justifyContent: 'space-between'}}>
           <Text style={{color: '#c4941d', fontSize: 24, paddingLeft: 5}}>{this.props.title}</Text>
-          <FlatList
-            horizontal={true} data={this.props.data}
-            renderItem={this._renderItem}
-            keyExtractor={(item, index) => index.toString()}
-          />
+          {content}
         </View>
       )
     }
