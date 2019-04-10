9fff8d16f99e4adc7fc3270307ef1908d264ab67
Jacob Phillips <jacob_phillips@student.uml.edu>
2019-04-10 08:37:05 -0400

    SettingsScreen implemented
    - run: npm install react-native-settings-page --save
    - screen not completely finished, some work still to be done

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import {View, Text, StyleSheet, AppRegistry} from 'react-native';
 import { MenuProvider } from 'react-native-popup-menu';
 import {
@@ -28,93 +28,58 @@ export default class SettingsScreen extends React.Component {
     }
 }
 
-AppRegistry.registerComponent('SettingsScreen', () => SettingsScreen);
+AppRegistry.registerComponent('SettingsScreen', () => SettingsScreen);*/
 
-
-/*import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, View, Text, Alert } from 'react-native';
+import ReactNativeSettingsPage, { 
+	SectionRow, 
+	NavigateRow,
+  CheckRow,
+  SwitchRow,
+  SliderRow
+} from 'react-native-settings-page';
 
 import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas'
 
 export default class SettingsScreen extends Component {
     static navigationOptions = {
-        title: 'Jakes Screen',
-        //headerRight: <HeaderCheckIcon />
+        title: 'Settings',
       };
+// TODO: implement your navigationOptions
+state = {
+  check: false,
+  switch: false,
+  value: 40
+}
+render() {
+  return (
+    <ReactNativeSettingsPage>
+      <SectionRow text='Settings'>  
+        <SwitchRow 
+          text=' DreamUp Notifications/Alerts' 
+          //iconName='your-icon-name'
+          _value={this.state.switch}
+          _onValueChange={() => { this.setState({ switch: !this.state.switch }) }} />
+        <CheckRow 
+          text='  DreamUp Airplane Mode'
+          //iconName='your-icon-name'
+          _color='#000'
+          _value={this.state.check}
+          _onValueChange={() => { this.setState({ check: !this.state.check }) }} />
+        <SliderRow 
+          text='DreamUp Volume'
+         // iconName='your-icon-name'
+          _color='#000'
+          _min={0}
+          _max={100}
+          _value={this.state.value}
+          _onValueChange={value => { this.setState({ value }) }} />
+      </SectionRow>
+    </ReactNativeSettingsPage>
+  )
+}
+}
 
-    render() {
-        return (
-            <View style={styles.container}>
-              <View style={{ flex: 1, flexDirection: 'row' }}>
-                <RNSketchCanvas
-                  containerStyle={{ backgroundColor: 'transparent', flex: 1 }}
-                  canvasStyle={{ backgroundColor: 'transparent', flex: 1 }}
-                  defaultStrokeIndex={0}
-                  defaultStrokeWidth={5}
-                  undoComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Undo</Text></View>}
-                  clearComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Clear</Text></View>}
-                  eraseComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Eraser</Text></View>}
-                  strokeComponent={color => (
-                    <View style={[{ backgroundColor: color }, styles.strokeColorButton]} />
-                  )}
-                  strokeSelectedComponent={(color, index, changed) => {
-                    return (
-                      <View style={[{ backgroundColor: color, borderWidth: 2 }, styles.strokeColorButton]} />
-                    )
-                  }}
-                  strokeWidthComponent={(w) => {
-                    return (<View style={styles.strokeWidthButton}>
-                      <View  style={{
-                        backgroundColor: 'white', marginHorizontal: 2.5,
-                        width: Math.sqrt(w / 3) * 10, height: Math.sqrt(w / 3) * 10, borderRadius: Math.sqrt(w / 3) * 10 / 2
-                      }} />
-                    </View>
-                  )}}
-                  saveComponent={<View style={[styles.functionButton, {backgroundColor: '#b300b3'}]}><Text style={{color: 'white'}}>Save</Text></View>}
-                  savePreference={() => {
-                    return {
-                      folder: 'Dreams',
-                      filename: String(Math.ceil(Math.random() * 100000000)),
-                      transparent: false,
-                      includeImage: false,
-                      includeText: false,
-                      cropToImageSize: false,
-                      imageType: 'jpg',
-                    }
-                  }}
-                  onSketchSaved={(success, path) => {
-                      // on Success, send path with nav to a new DreamScreen
-                      // on Fail, provide message and don't do anything
-                      if (success) {
-                          this.props.navigation.navigate("DreamFragmentScreen", {visionPath: path, existing: false});
-                      } else {
-                        Alert.alert('ERROR','Failed to save image!');
-                      }
-                  }}
-                />
-              </View>
-            </View>
-          );
-        }
-      }
-
-      const styles = StyleSheet.create({
-        container: {
-          flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAE5FB',//F5FCFF
-        },
-        strokeColorButton: {
-          marginHorizontal: 2.5, marginVertical: 8, width: 30, height: 30, borderRadius: 15,
-        },
-        strokeWidthButton: {
-          marginHorizontal: 2.5, marginVertical: 8, width: 30, height: 30, borderRadius: 15,
-          justifyContent: 'center', alignItems: 'center', backgroundColor: '#2b1381'
-        },
-        functionButton: {
-          marginHorizontal: 2.5, marginVertical: 8, height: 30, width: 60,
-          backgroundColor: '#c4941d', justifyContent: 'center', alignItems: 'center', borderRadius: 5,
-        }
-      });
-
-      AppRegistry.registerComponent('SettingsScreen', () => SettingsScreen);*/
-
-
+       
+      AppRegistry.registerComponent('SettingsScreen', () => SettingsScreen);
