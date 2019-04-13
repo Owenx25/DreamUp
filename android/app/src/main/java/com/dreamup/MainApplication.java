1bfa05c5278348c38ea3d644aacfac66031b68a3
Jacob Phillips <jacob_phillips@student.uml.edu>
2019-04-13 11:26:29 -0400

    react-native-push-notification added
    Link: https://github.com/zo0r/react-native-push-notification
    
    - npm install --save react-native-push-notification
    - react-native link react-native-push-notification
    
    -still need to update AndroidManifest.xml and implement fully

--- android/app/src/main/java/com/dreamup/MainApplication.java
+++ android/app/src/main/java/com/dreamup/MainApplication.java
@@ -3,6 +3,7 @@ package com.dreamup;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.terrylinla.rnsketchcanvas.SketchCanvasPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNFirebasePackage(),
             new AsyncStoragePackage(),
             new SketchCanvasPackage(),
